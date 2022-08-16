b62bce452cbd9073d37a286ba31cf70556643f6e
lorenzreweghs <lorenz.r99@gmail.com>
2022-08-16 11:54:20 +0200

    feat: add userRouter + first GET route

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { collections } from './services/database.service';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const userRouter = express.Router();
+
+app.use(express.json());
+
+userRouter.get('/', async (_req: Request, res: Response) => {
+  try {
+    // const users = (await collections.users?.find({}).toArray()) as User[];
+    const users = await collections.users?.find({}).toArray();
+
+    res.status(200).send(users);
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
 });
+
+app.use('/api', userRouter);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
