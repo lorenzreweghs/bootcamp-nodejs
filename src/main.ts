211b89ed2978e04ea73515613faefedf6c56221b
lorenzreweghs <lorenz.r99@gmail.com>
2022-08-17 10:29:09 +0200

    refactor: remove creation of app to seperate file

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,11 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'dotenv/config';
-import express from 'express';
 
-import authRouter from './routes/auth';
-import userRouter from './routes/user';
+import createServer from './server';
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-
-app.use(express.json());
-
-app.use('/api', authRouter);
-app.use('/api', userRouter);
+const app = createServer();
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
