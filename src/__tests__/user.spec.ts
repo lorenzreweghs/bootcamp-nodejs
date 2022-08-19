d2031e2fed028a310112a26c121c9f9dc0ec4796
lorenzreweghs <lorenz.r99@gmail.com>
2022-08-19 14:25:17 +0200

    test(user): hash passwords + fix refreshToken test

--- src/__tests__/user.spec.ts
+++ src/__tests__/user.spec.ts
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt';
+import { ObjectId } from 'mongodb';
 import supertest from 'supertest';
 
 import createServer from '../server';
@@ -6,39 +8,76 @@ import { collections, connectToDatabase } from '../services/database.service';
 describe('users', () => {
   const app = createServer();
 
-  const user = {
-    username: 'Lorenz',
+  const userInput = {
+    firstName: 'Lorenz',
+    lastName: 'Reweghs',
+    email: 'lorenz.reweghs@euri.com',
     password: 'Znerol',
+    role: 'user',
   };
 
-  describe('user registration', () => {
-    beforeAll(async () => {
-      await connectToDatabase().connect();
+  beforeAll(async () => {
+    await connectToDatabase().connect();
+    try {
       await collections.users?.drop();
-    });
+      await collections.tokens?.drop();
 
-    afterAll(async () => {
-      await connectToDatabase().disconnect();
-    });
+      bcrypt.hash('Kcarab', 10, async (err, hash) => {
+        await collections.users?.insertOne({
+          firstName: 'Barack',
+          lastName: 'Obama',
+          email: 'barack.obama@euri.com',
+          password: hash,
+          role: 'user',
+        });
+      });
 
+      bcrypt.hash('Eoj', 10, async (err, hash) => {
+        await collections.users?.insertOne({
+          firstName: 'Joe',
+          lastName: 'Biden',
+          email: 'joe.biden@euri.com',
+          password: hash,
+          role: 'admin',
+        });
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  afterAll(async () => {
+    await connectToDatabase().disconnect();
+  });
+
+  describe('user registration', () => {
     describe('given the user signs up with a username and password', () => {
-      it('should return statusCode 201 and an object with the ID and username', async () => {
-        const { statusCode, body } = await supertest(app).post('/api/users').send(user);
+      it('should return statusCode 201 and the user without the password', async () => {
+        const { statusCode, body } = await supertest(app).post('/api/users').send(userInput);
 
+        const { password, ...userOutput } = userInput;
         expect(statusCode).toBe(201);
         expect(body).toEqual({
           id: expect.any(String),
-          username: 'Lorenz',
+          ...userOutput,
         });
 
-        const userDoc = await collections.users?.findOne({ username: 'Lorenz' });
+        const userDoc = await collections.users?.findOne({ _id: new ObjectId(body.id) });
         expect(userDoc).toBeDefined();
+        expect(userDoc).toEqual({
+          _id: new ObjectId(body.id),
+          ...userInput,
+          password: expect.any(String),
+        });
       });
     });
 
     describe('given the username and password are valid', () => {
       it('should return an accessToken and refreshToken and save the refreshToken', async () => {
-        const { statusCode, body } = await supertest(app).post('/api/login').send(user);
+        const { statusCode, body } = await supertest(app).post('/api/login').send({
+          email: userInput.email,
+          password: userInput.password,
+        });
 
         expect(statusCode).toBe(200);
         expect(body).toEqual({
@@ -46,13 +85,35 @@ describe('users', () => {
           refreshToken: expect.any(String),
         });
 
-        // check if refreshToken is saved
+        const { refreshToken } = body;
+        const token = await collections.tokens?.findOne({ refreshToken });
+        expect(token).toBeDefined();
+        expect(token).toEqual({
+          _id: expect.any(ObjectId),
+          refreshToken,
+          user_id: expect.any(ObjectId),
+        });
+      });
+    });
+
+    describe('given the email and password do not match', () => {
+      it('should return statusCode 403', async () => {
+        const { statusCode, body } = await supertest(app).post('/api/login').send({
+          email: userInput.email,
+          password: 'abc',
+        });
+
+        expect(statusCode).toBe(403);
+        expect(body).toEqual({});
       });
     });
 
     describe('given the accessToken is expired but the refreshToken is valid', () => {
       it('should return a new accessToken', async () => {
-        const result = await supertest(app).post('/api/login').send(user);
+        const result = await supertest(app).post('/api/login').send({
+          email: userInput.email,
+          password: userInput.password,
+        });
         const { refreshToken } = result.body;
 
         const { statusCode, body } = await supertest(app).post('/api/token').send({ refreshToken });
@@ -68,7 +129,7 @@ describe('users', () => {
       it('should return statusCode 401', async () => {
         const { statusCode, body } = await supertest(app).post('/api/token').send({});
 
-        expect(statusCode).toBe(401);
+        expect(statusCode).toBe(400);
         expect(body).toEqual({});
       });
     });
@@ -86,13 +147,19 @@ describe('users', () => {
 
     describe('given the user logs out with a refreshToken', () => {
       it('should return statusCode 204 and remove the refreshToken', async () => {
-        const { statusCode } = await supertest(app)
-          .delete('/api/logout')
-          .send({ refreshToken: 'avbsou47nbcijs35cnao' });
+        const result = await supertest(app).post('/api/login').send({
+          email: 'barack.obama@euri.com',
+          password: 'Kcarab',
+        });
+        const { refreshToken } = result.body;
+        expect(result.statusCode).toBe(200);
+
+        const { statusCode } = await supertest(app).delete('/api/logout').send({ refreshToken });
 
         expect(statusCode).toBe(204);
 
-        // check if refreshToken is removed
+        const token = await collections.tokens?.findOne({ refreshToken });
+        expect(token).toBeNull();
       });
     });
 
