4f7c114922c5f8110ba9b767517d2f2088a173be
lorenzreweghs <lorenz.r99@gmail.com>
2022-08-24 11:51:04 +0200

    refactor(product): error handling + check deleted/modified count

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -2,72 +2,77 @@
 import express from 'express';
 import { ObjectId } from 'mongodb';
 import { authenticateToken } from '../middleware/auth';
+import Product from '../models/product';
 import { collections } from '../services/database.service';
 
 const productRouter = express.Router();
 
 productRouter
   .route('/products')
-  .get(async (_req, res) => {
+  .get(async (_req, res, next) => {
     try {
-      const products = await collections.products?.find({}).toArray();
+      const products = await collections.products?.find<Product[]>({}).toArray();
 
       res.status(200).send(products);
-    } catch (error: any) {
-      res.send(500).send(error.message);
+    } catch (error) {
+      next(error);
     }
   })
-  .post(authenticateToken, async (_req, res) => {
+  .post(authenticateToken, async (_req, res, next) => {
     try {
       const result = await collections.products?.insertOne({ ..._req.body });
 
-      res.send(201).json({
-        id: result?.insertedId,
+      res.status(201).json({
         ..._req.body,
+        id: result?.insertedId,
       });
     } catch (error) {
-      res.sendStatus(403);
+      next(error);
     }
   });
 
 productRouter
   .route('/products/:id')
-  .get(async (_req, res) => {
+  .get(async (_req, res, next) => {
     const { id } = _req.params;
 
     try {
-      const product = await collections.products?.findOne({ _id: new ObjectId(id) });
+      const product = await collections.products?.findOne<Product>({ _id: new ObjectId(id) });
 
       if (!product) return res.sendStatus(404);
 
       res.status(200).send(product);
-    } catch (error: any) {
-      res.status(500).send(error.message);
+    } catch (error) {
+      next(error);
     }
   })
-  .put(authenticateToken, async (_req, res) => {
+  .put(authenticateToken, async (_req, res, next) => {
     const { id } = _req.params;
 
     try {
-      await collections.products?.updateOne({ _id: new ObjectId(id) }, { $set: { ..._req.body } });
+      const result = await collections.products?.updateOne({ _id: new ObjectId(id) }, { $set: { ..._req.body } });
+
+      if (!result?.modifiedCount) return res.sendStatus(404);
 
       res.status(200).json({
-        id,
         ..._req.body,
+        id,
       });
-    } catch (error: any) {
-      res.status(403).send(error.message);
+    } catch (error) {
+      next(error);
     }
   })
-  .delete(authenticateToken, async (_req, res) => {
+  .delete(authenticateToken, async (_req, res, next) => {
     const { id } = _req.params;
 
     try {
-      await collections.products?.deleteOne({ _id: new ObjectId(id) });
+      const result = await collections.products?.deleteOne({ _id: new ObjectId(id) });
+
+      if (!result?.deletedCount) return res.sendStatus(404);
 
       res.status(200).send(`Product with ID ${id} deleted`);
-    } catch (error: any) {
-      res.status(500).send(error.message);
+    } catch (error) {
+      next(error);
     }
   });
 
